dc91282e2e0ab907a2985eabc232842971c07d66
Shisan <1836600741@qq.com>
2023-08-31 12:21:40 +0800

    自动清除打包目录插件的使用 clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 // 引用自动创建HTML模板插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 自动清除打包目录 插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 // 路径处理
 function reslovePath(dirName = '') {
     if (typeof dirName !== 'string') return Error('Type Error ,Please enter String Type')
@@ -18,14 +20,17 @@ module.exports = {
     // 出口文件
     output: {
         path: reslovePath('dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.[hash:7].js'
     },
     // 插件配置
     plugins: [
+        // 创建模板
         new HtmlWebpackPlugin({
             template: reslovePath('./public/index.html'),
             path: reslovePath('dist/index.html'),
             filename: 'index.html'
-        })
+        }),
+        // 自动清除打包目录
+        new CleanWebpackPlugin()
     ]
 }
