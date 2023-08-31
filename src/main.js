ff8f9218877c1537e6afd304f650b980ddb3a076
Shisan <1836600741@qq.com>
2023-08-31 14:29:08 +0800

    当前w5,采用内置的(asset Module Type)对资源文件进行处理，背景图自适应，简单重置css样式

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import './styles/theme.css'
+// import './assets/images/cat.jpg'
+import './styles/reset.css'
+let img = require('./assets/images/cat.jpg')
+console.log(img)
 console.log('配置')
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 // 自动清除打包目录 插件
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { Parser } = require('webpack')
 // 路径处理
 function reslovePath(dirName = '') {
     if (typeof dirName !== 'string') return Error('Type Error ,Please enter String Type')
@@ -20,16 +21,66 @@ module.exports = {
     // 出口文件
     output: {
         path: reslovePath('dist'),
-        filename: 'bundle.[hash:7].js'
+        filename: 'bundle.[hash:7].js',
+        // assetModuleFilename: 'images/[hash][ext]'
     },
     // 打包规则处理
     module: {
         rules: [
             // css文件处理
             {
-                test: /.css$/,
+                test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            //#region
+            /**  
+             * @description {file-loader,url-loader}
+             * 在webpack5的官网中url-loader，file-loader已经是弃用了的，通过资源模块类型(asset module type)替换掉了webpack5之前的loader
+             * 如果还要使用这两个废弃了的文件的话，必须加上最后那一句type: ‘javascript/auto’,
+             * 否则会出现一张图片打包两次，而且会出现背景图片不会显示到页面上的问题
+             */
+            // webpack5之前的文件处理
+            // {
+            //     test: /\.(png|jpe?g|gif|svg|webp)$/i,
+            //     loader: 'file-loader',
+            //     options: {
+            //         outputPath: 'images',
+            //         name: '[name].[hash:7].[ext]',
+            //         esModule: false,
+            //         type: 'javascript/auto'
+            //     },
+            // },
+            // {
+            //     test: /\.(png|jpe?g|gif|svg|webp)$/i,
+            //     loader: 'url-loader',
+            //     options: {
+            //         publicPath: './images', //相对打包后的index.html的图片位置
+            //         outputPath: 'images', //打包成功后图片的文件夹名字
+            //         // 解决：关闭url-loader的es6模块化，使用commonjs解析
+            //         esModule: false,
+            //         // 图片大小超出10kb 会被转化为base64
+            //         limit: 120 * 1024,
+            //         // 图片重命名[name]:站位 [hash:6]代指哈希值站6位,[ext]:匹配图片的后缀名 列如.(png|jpe?g|gif|svg|webp或其他文件后缀名等
+            //         name: '[hash:7].[name].[ext]',
+            //     },
+            //     type: 'javascript/auto'
+            // },
+            // #endregion
+            //使用webpack5 内置资源类型处理器进行处理(asset module type)
+            {
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
+                type: 'asset', // 当不符合条件时会自动走asset/resource模块规则处理，符合则会遵循asset/inline规则处理相当于
+                generator: {
+                    filename: 'images/[name].[hash:7][ext]',
+                },
+                // 对于base64的处理
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 1024 * 120 //小于120kb 转化为base64
+                    }
+                }
             }
+
         ],
     },
 
